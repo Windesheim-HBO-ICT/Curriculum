cd25dfc0d5293dc2cf46e24ed2cfa9a0b27c1096
jorrickstempher <s1172166@student.windesheim.nl>
2024-05-08 13:45:26 +0200

    No bug fixes, only more bugs added
    Need to fix the issue with setting the classnames.

--- components/web-navbar.js
+++ components/web-navbar.js
@@ -8,10 +8,11 @@ template.innerHTML = `
 `;
 
 class WebNavbar extends HTMLElement {
+  iteration;
   firstTime;
   shadowRoot;
 
-  attachStyling(){
+  attachStyling() {
     const linkElem = document.createElement("link");
     linkElem.setAttribute("rel", "stylesheet");
     linkElem.setAttribute("href", "/css/web-navbar.css");
@@ -21,17 +22,17 @@ class WebNavbar extends HTMLElement {
   constructor() {
     super();
     this.firstTime = true;
+    this.iteration = 1;
     this.shadowRoot = this.attachShadow({ mode: "open" });
     import("/data/architectuurlaag/se/curriculum.js").then((module) => {
       this.buildTree(module.default, this.shadowRoot, this.firstTime);
     });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.attachStyling();
-
   }
 
   buildTree(data, parent, firstTime) {
-    const ul = document.createElement("ul");
+    const ul = document.createElement("div");
     if (firstTime) {
       ul.classList.add("ssldc-items");
       firstTime = false;
@@ -41,36 +42,52 @@ class WebNavbar extends HTMLElement {
     parent.appendChild(ul);
 
     data.forEach((item) => {
-      const ssldc_li = this.shadowRoot.appendChild(document.createElement("li"));
+      const ssldc_li = this.shadowRoot.appendChild(
+        document.createElement("div")
+      );
+      console.log(this.iteration);
+      if (this.iteration === 1) {
+        ssldc_li.classList.add("ssldc-item");
+      } else if (this.iteration === 2) {
+        ssldc_li.classList.add("hbo-i-activiteit");
+      } else if (this.iteration === 3) {
+        ssldc_li.classList.add("hbo-i-item");
+      }
       //on first time: add ssldc-item
       // on second time: add hbo-i-item
       // on third time: addh hbo-i-activiteit
-      ssldc_li.classList.add("ssldc-item");
+      // ssldc_li.classList.add("ssldc-item");
       ssldc_li.textContent = item.naam;
       ul.appendChild(ssldc_li);
 
       if (item.vaardigheden) {
-        const vaardighedenUl =  this.shadowRoot.appendChild(document.createElement("ul"));
-        vaardighedenUl.classList.add("hbo-i-activeiten");
+        const vaardighedenUl = this.shadowRoot.appendChild(
+          document.createElement("div")
+        );
+        vaardighedenUl.classList.add("hbo-i-activiteiten");
         ssldc_li.appendChild(vaardighedenUl);
 
         item.vaardigheden.forEach((vaardigheid) => {
-          const vaardigheidLi =  this.shadowRoot.appendChild(document.createElement("li"));
-          vaardigheidLi.classList.add("hbo-i-activiteit");
+          const vaardigheidLi = this.shadowRoot.appendChild(
+            document.createElement("div")
+          );
+          vaardigheidLi.classList.add("hbo-i-vaardigheid");
 
           vaardigheidLi.textContent = vaardigheid.naam;
           vaardighedenUl.appendChild(vaardigheidLi);
 
           if (vaardigheid.vaardigheden) {
             this.buildTree(vaardigheid.vaardigheden, vaardighedenUl, firstTime);
+            this.iteration = 3;
           }
         });
       }
-      
       if (item.labels) {
+        this.iteration = 3;
         this.buildTree(item.labels, ssldc_li, firstTime);
       }
     });
+    this.iteration = 1;
   }
 }
 
