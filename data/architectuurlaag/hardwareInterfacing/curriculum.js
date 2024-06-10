f998ba2205c3b837db084722b30b4533df4d99ef
FemkeSpanner <90692319+FemkeSpanner@users.noreply.github.com>
2024-06-10 11:09:40 +0200

    Revert "Updated desktop version"

--- data/architectuurlaag/hardwareInterfacing/curriculum.js
+++ data/architectuurlaag/hardwareInterfacing/curriculum.js
@@ -8,27 +8,22 @@ const hardwareInterfacingCurriculum = [
   {
     naam: "SSDLC Requirements1",
     labels: requirements,
-    backgroundColor: "rgba(26, 191, 161, 0.75)"
   },
   {
     naam: "SSDLC Design",
     labels: design,
-    backgroundColor: "rgba(56, 141, 205, 0.75)"
   },
   {
     naam: "SSDLC Development",
     labels: development,
-    backgroundColor: "rgba(236, 165, 75, 0.75)"
   },
   {
     naam: "SSDLC Testing",
     labels: testing,
-    backgroundColor: "rgba(225, 67, 152, 0.75)"
   },
   {
     naam: "SSDLC Deployment",
     labels: deployment,
-    backgroundColor: "rgba(239, 115, 63, 0.75)"
   },
 ];
 
